b80957686347eb5c9a9b17019d4a0b1133b859ba
zhaobo <xtype@qq.com>
2020-12-07 14:23:48 +0800

    Fix

--- examples/normal/client.go
+++ examples/normal/client.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/myxtype/go-gateway/client"
+import (
+	"github.com/myxtype/go-gateway/client"
+	"github.com/myxtype/go-gateway/pkg/logger"
+)
 
 func main() {
 	for i := 0; i < 1000; i++ {
-		conn := client.NewAsyncTcpConnection("127.0.0.1:2000")
-		go conn.Connect()
+		go func(i int) {
+			logger.Sugar.Info(i)
+			conn := client.NewAsyncTcpConnection("127.0.0.1:2000")
+			conn.Connect()
+		}(i)
 	}
 
 	select {}
